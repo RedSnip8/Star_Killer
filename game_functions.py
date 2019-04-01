4c1d5c7345594295b0ca3dbbcb91b99a85e41926
Redsnip8 <FCipolone@gmail.com>
2019-04-01 18:43:23 -0400

    Added game_funcions Module

---
+++ game_functions.py
@@ -0,0 +1,56 @@
+import sys
+import pygame
+
+from bullets import Player_Laser
+
+
+def check_keyDown(event, ai_settings, screen, player, projectiles):
+    """Reactions to Key presses"""
+    if event.key == pygame.K_UP:
+        player.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        player.moving_down = True
+    elif event.key == pygame.K_SPACE:
+        player.moving_right = True
+        player.not_moving = False
+    elif event.key == pygame.K_v:
+        # Create a new laser and add it to the projectiles group
+        new_laser = Player_Laser(ai_settings, screen, player)
+        projectiles.add(new_laser)
+
+
+def check_keyUp(event, player):
+    """Reactions to Key Releases"""
+    if event.key == pygame.K_UP:
+        player.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        player.moving_down = False
+    elif event.key == pygame.K_SPACE:
+        player.moving_right = False
+        player.not_moving = True
+
+
+def check_player_events(ai_settings, screen, player, projectiles):
+    """Keyboard and mouse watcher. Will Repsond to inputs"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            check_keyDown(event, ai_settings, screen, player, projectiles)
+        elif event.type == pygame.KEYUP:
+            check_keyUp(event, player)  
+
+
+
+def update_screen(conductor_settings, screen, star_killer, projectiles):
+    """Redraw the screen during each pass"""
+    screen.fill(conductor_settings.bg_color)
+    # Redraw all projectiles behind enemy ships
+    for laser in projectiles.sprites():
+        laser.draw_laser()
+
+    star_killer.blitme()
+
+    # Make the most recently drawn screen visible
+    pygame.display.flip()
