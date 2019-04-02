cc9660746fdd8ae59b8b72e9714a182a981977b3
Redsnip8 <FCipolone@gmail.com>
2019-04-02 19:10:38 -0400

    Removed sprite_sheet.py class

--- sprite_sheet.py
+++ sprite_sheet.py
@@ -1,30 +1,24 @@
-import pygame
+import pygame, math
 
 class SpriteSheeter():
-    def __int__(self, filename, columns, rows):
-        self.sheet = pygame.image.load(filename).covert_alpha()
+    def __init__(self, filename, columns, rows):
+        self.sheet = pygame.image.load(filename).convert_alpha()
 
         self.columns = columns
         self.rows = rows
         self.totalcells = columns * rows   
 
         self.rect = self.sheet.get_rect()
-        width = self.cellWidth = self.rect.width / columns
-        height = self.cellHieght = self.rect.height / rows
-        hwidth, hheight = self.cellCenter = (width / 2, height / 2)
+        cwidth = self.cellWidth = self.rect.width / columns
+        cheight = self.cellHieght = self.rect.height / rows
 
         self.cells = list(
             [
-                (index % columns * width, index / columns * height)
-                for index in ranges(self.totalcells)
+                (index % columns * cwidth, index / columns * cheight, cwidth, cheight)
+                for index in range(self.totalcells)
             ]
                         )
-    
-        self.handle = list([
-            (0, 0), (-hwidth, 0), (-width, 0),
-            (0, hheight), (-hwidth, -hheight), (-width, -hheight)
-            (0, -height), (-hwidth, -height), (-width, -height),
-        ])
 
-    def draw(self, surface, cellindex, x, y, handle = 0):
-        surface.blit( self.sheet, (x + self.handle[handle][0], y + self.handle[handle][1], self.cells[cellindex])
+
+    def draw(self, surface, cellindex):
+        surface.blit( self.sheet, (self.cells[cellindex]))
